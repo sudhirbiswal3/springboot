1872de0eb3376b9284709b3e3e8566280184bfa8
unknown <bsudhir.biswal3@gmail.com>
2019-10-05 14:43:03 +0400

    Changed 1 with comment

--- springboot2-jpa-crud-example/src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
+++ springboot2-jpa-crud-example/src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //http://localhost:4200/add	
+/*
+ * Root Application for spring boot
+ */
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
