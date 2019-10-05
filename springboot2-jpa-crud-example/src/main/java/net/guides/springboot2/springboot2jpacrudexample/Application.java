e306e90606d4540e83572a86b9fed6b8bca42425
sudhirbiswal3 <44292505+sudhirbiswal3@users.noreply.github.com>
2019-10-05 13:09:14 +0400

    Adding Spring Boot Public files

---
+++ springboot2-jpa-crud-example/src/main/java/net/guides/springboot2/springboot2jpacrudexample/Application.java
@@ -0,0 +1,12 @@
+package net.guides.springboot2.springboot2jpacrudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//http://localhost:4200/add	
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
